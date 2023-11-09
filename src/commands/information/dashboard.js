65c859215ad4abd3c409cd86d4ca12a64857acda
13Thomas <skatim@yahoo.com>
2023-11-09 12:39:37 +0200

    w

--- src/commands/information/dashboard.js
+++ src/commands/information/dashboard.js
@@ -1,5 +1,5 @@
 const Command = require("../../structures/Command");
-const { MessageEmbed } = require("discord.js");
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -10,13 +10,42 @@ module.exports = class extends Command {
       cooldown: 3,
     });
   }
+
   async run(message) {
     const dashembed = new MessageEmbed()
       .setTitle("Need the bot's dashboard link? Here you go!")
       .setDescription("https://marksoft.13thomasbot.repl.co/")
       .setColor("RANDOM")
-      .setFooter({ text: `Requested by ${message.author.username}` })
+      .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
-    message.channel.sendCustom({ embeds: [dashembed] });
+
+    const inviteButton = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Invite")
+      .setURL("https://marksoft.13thomasbot.repl.co/invite");
+
+    const supportButton = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Support Server")
+      .setURL("https://marksoft.13thomasbot.repl.co/support");
+
+    const dashboardButton = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("Dashboard")
+      .setURL("https://marksoft.13thomasbot.repl.co/");
+
+    const devTeamButton = new MessageButton()
+      .setStyle("LINK")
+      .setLabel("DevTeam")
+      .setURL("https://marksoft.13thomasbot.repl.co/team");
+
+    const row = new MessageActionRow().addComponents(
+      inviteButton,
+      supportButton,
+      dashboardButton,
+      devTeamButton
+    );
+
+    message.channel.send({ embeds: [dashembed], components: [row] });
   }
-}
+};
