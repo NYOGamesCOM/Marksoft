fe5a724f32161e7acc60f0b0f66f7741b35e675a
13Thomas <skatim@yahoo.com>
2024-06-03 12:26:31 +0300

    update

--- src/commands/information/naughty.js
+++ src/commands/information/naughty.js
@@ -14,18 +14,15 @@ module.exports = class extends Command {
     }
 
     async run(message, args) {
-        // Generate a random number between 1 and 69
+
         const randomNumber = Math.floor(Math.random() * 69) + 1;
 
-        // Create the base message
         let responseMessage = `**${message.author.username}** is **${randomNumber}** out of **69** naughty :KEKW: \n`;
 
-        // Add a congratulatory message if the number is 69
         if (randomNumber === 69) {
             responseMessage += '\n🎉 Congratulations! You hit the lucky number 69! 🎉';
         }
 
-        // Create the embed message
         const embed = new MessageEmbed()
             .setTitle('Naughty calculator 101')
             .setDescription(responseMessage)
@@ -36,8 +33,7 @@ module.exports = class extends Command {
                 iconURL: message.author.displayAvatarURL({ dynamic: true }),
             })
             .setTimestamp();
-
-        // Send the embed message to the channel
+            
         await message.channel.send({ embeds: [embed] });
         message.delete().catch(err => console.error('Failed to delete the message:', err));
     }
